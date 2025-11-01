25c2d9bd0e370e363cc066164e58253a2c0c6dbe
juan-cely-l <juan.cely-l@mail.escuelaing.edu.co>
2025-10-31 21:25:13 -0500

    add:Maven Structure and Frontend

---
+++ src/main/java/com/example/TicTacToeParcial/TicTacToeParcialApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicTacToeParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TicTacToeParcial/TicTacToeParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicTacToeParcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
